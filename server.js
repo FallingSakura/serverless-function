c911b7dd795aa5f2808630005a5d9e6280874725
FallingSakura <dbw1732351264@163.com>
2024-09-02 11:17:58 +0800

    path

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const path = require('path')
 // const DIR =
 //   '/home/fallingsakura/CodeProgram/Front-End/Apps/Portfolio/FallingWeb/src/'
 // const dataPath = path.join(DIR, './data/calendar.json')
-const dataPath = './src/calendar.json'
+const dataPath = path.join(__dirname, './src/calendar.json')
 const cors = require('cors')
 
 app.use(cors())
@@ -28,6 +28,7 @@ app.post('/update-data', (req, res) => {
   res.send('Success')
 })
 app.get('/', (req, res) => {
+  console.log(dataPath)
   res.send('Hello World')
 })
 app.get('/get-data', (req, res) => {

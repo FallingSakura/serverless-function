e38f9701e1c8567449f45739d7af4de5f2286fea
FallingSakura <dbw1732351264@163.com>
2024-09-16 22:35:15 +0800

    Login

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 require('dotenv').config()
-const fs = require('fs')
 const path = require('path')
 const express = require('express')
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
 const cors = require('cors')
 const { MongoClient, ObjectId } = require('mongodb')
 const app = express()
@@ -24,7 +25,8 @@ async function connectToDatabase() {
 
   app.use(cors())
   app.use(express.json()) // 中间件，将 JSON 请求体转换为对象
-  app.put('/update-data/:id', async (req, res) => {
+  app.put('/update-data', authenticateToken, async (req, res) => {
+    res.send("haha")
     const id = req.params.id
     const { date, value } = req.body
     try {
@@ -37,14 +39,28 @@ async function connectToDatabase() {
     }
     res.send('Update-data Success')
   })
-  app.get('/get-data/:id', async (req, res) => {
+  app.get('/get-data', authenticateToken, async (req, res) => {
     const id = req.params.id
     const document = await collection.findOne({ _id: new ObjectId(id) })
     await clearData(document, id)
     res.json(document.data)
   })
+  app.post('/login', async (req, res) => {
+    const { email, password } = req.body
+    const user = await collection.findOne({ email: email })
+    if (!user) {
+      return res.status(400).json({ message: 'User not found.' })
+    }
+    // const isPasswordValid = await bcrypt.compare(password, user.data.password)
+    const isPasswordValid = (password === user.password)
+    if (!isPasswordValid) {
+      return res.status(400).json({ message: 'Password Error.' })
+    }
+    const token = jwt.sign({ id: user.data._id }, 'hahaha', {expiresIn: '24h' })
+    res.json({ token })
+  })
   app.listen(port, '0.0.0.0', () => {
-    console.log(`Server running at https://localhost:${port}`)
+    console.log(`Server running at http://localhost:${port}`)
   })
 }
 
@@ -63,4 +79,16 @@ async function clearData(document, id) {
   }
   if (Object.keys(keysToUnset).length === 0) return
   await collection.updateOne({ _id: new ObjectId(id) }, { $unset: keysToUnset })
+}
+
+function authenticateToken(req, res, next) {
+  const authHeader = req.headers['authorization']
+  const token = authHeader && authHeader.split(' ')[1]
+  if (!token) return res.status(401).json({ message: 'No Token.' })
+
+  jwt.verify(token, 'hahaha', (err, user) => {
+    if (err) return res.status(403).json({ message: 'Token Invalid.' })
+      req.user = user
+      next()
+  })
 }
